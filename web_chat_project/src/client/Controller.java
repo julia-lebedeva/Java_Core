bec968e5d7348a135896d5670bed22b6c2949f91
Yulia Lebedeva <>
2019-10-17 23:43:02 +0300

    added action on button

--- web_chat_project/src/client/Controller.java
+++ web_chat_project/src/client/Controller.java
@@ -1,4 +1,23 @@
 package client;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+
 public class Controller {
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    TextField textField;
+
+    public void sendMsg() {
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+    }
+
+
 }
