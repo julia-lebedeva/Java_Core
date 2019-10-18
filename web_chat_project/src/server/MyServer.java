5289c4d8b6350030f2c97d42ba4f88fc43c582cc
Yulia Lebedeva <>
2019-10-18 18:20:16 +0300

    added code for MyServer

--- web_chat_project/src/server/MyServer.java
+++ web_chat_project/src/server/MyServer.java
@@ -1,4 +1,46 @@
 package server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class MyServer {
+    public static void main(String[] args) {
+        Socket socket = null;
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(8189);
+            System.out.println("Server started");
+
+            socket = server.accept();
+            System.out.println("Client connected");
+
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            while (true) {
+                String str = in.readUTF();
+                if (str.equalsIgnoreCase("/end")) {
+                    break;
+                }
+                out.writeUTF("echo " + str);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
