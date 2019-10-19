0a4c952a252154a77a81b5e2aa0c2e3cdf544012
Yulia Lebedeva <>
2019-10-19 22:48:14 +0300

    added constructor for ClientHandler

--- web_chat_project/src/server/ClientHandler.java
+++ web_chat_project/src/server/ClientHandler.java
@@ -1,5 +1,24 @@
 package server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class ClientHandler {
-    
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private MyServer serv;
+
+    public ClientHandler(MyServer serv, Socket socket) {
+        try {
+            this.serv = serv;
+            this.socket = socket;
+            this.in = new DataInputStream(socket.getInputStream());
+            this.out = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
