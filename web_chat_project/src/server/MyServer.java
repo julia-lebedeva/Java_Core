83c50ca6047a7ca8cd396595d29f2d411cfa01f8
Yulia Lebedeva <>
2019-10-21 19:49:55 +0300

    added new class ServerStart

--- web_chat_project/src/server/MyServer.java
+++ web_chat_project/src/server/MyServer.java
@@ -7,15 +7,19 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class MyServer {
-    public static void main(String[] args) {
+    public void MyServ() {
         Socket socket = null;
         ServerSocket server = null;
         try {
             server = new ServerSocket(8189);
             System.out.println("Server started");
 
-            socket = server.accept();
-            System.out.println("Client connected");
+// подключение клиентов
+            while (true) {
+                socket = server.accept();
+                System.out.println("Client connected");
+                new ClientHandler(this, socket);
+            }
 
 //            DataInputStream in = new DataInputStream(socket.getInputStream());
 //            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
